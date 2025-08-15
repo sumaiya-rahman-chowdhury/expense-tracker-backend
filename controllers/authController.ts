f202c9141ad12b23ae9ce90c9de78745455c43b2
sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2025-08-16 05:42:22 +0600

    added authController

---
+++ controllers/authController.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from "express";
+import User from "../models/userModel";
+import generateToken from "../utils/generateToken";
+
+export const registerUser = async (req: Request, res: Response) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const user = await User.create({ name, email, password });
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken((user._id as string).toString()),
+    });
+  } catch (error) {
+    res.status(500).json({ message: (error as Error).message });
+  }
+};
+
+export const loginUser = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (user && (await user.comparePassword(password))) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken((user._id as string).toString()),
+      });
+    } else {
+      res.status(401).json({ message: "Invalid email or password" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: (error as Error).message });
+  }
+};
